4278dada3a7d8da028965018b31cd538af5da393
Junichi TSURUKAWA <j.tsurukawa@gmail.com>
2015-11-04 20:44:32 +0900

    Initial Commit

---
+++ MyToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyToDo
+//
+//  Created by El Capitan on 2015/11/04.
+//  Copyright © 2015年 Junichi Tsurukawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
